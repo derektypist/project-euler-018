117d70c17b57df9dc2ba9fe24ce41264140b54ed
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-20 10:20:29 +0000

    Completed function maximumPathSumI at script.js

--- js/script.js
+++ js/script.js
@@ -8,5 +8,11 @@ let txt = "";
 */
 function maximumPathSumI(triangle) {
     const triangleCopy = triangle.map(row => [...row]);
-    for (let )
+    for (let i=triangleCopy.length-2;i>=0;i--) {
+        for (let j=0;j<=i;j++) {
+            triangleCopy[i][j] = Math.max(triangleCopy[i+1][j],triangleCopy[i+1][j+1]);
+        }
+    }
+
+    return triangleCopy[0][0];
 }
