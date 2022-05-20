ceb6831af8cabb7494666b656351d71a6cfce483
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-20 10:16:12 +0000

    Added variable and function at script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Set Up Variable
+let txt = "";
+
+/* 
+    Function to find the maximum total in a triangle as
+    described in https://projecteuler.net/problem=18
+
+*/
+function maximumPathSumI(triangle) {
+    const triangleCopy = triangle.map(row => [...row]);
+    for (let )
+}
